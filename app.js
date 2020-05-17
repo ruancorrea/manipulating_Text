61b98e630bb7a5fa0a2052145603e858b276820e
Luan Gabriel <rhcs@ic.ufal.br>
2020-05-17 13:53:58 -0300

    make it better

---
+++ app.js
@@ -0,0 +1,38 @@
+// Carregando modulos
+    const express = require("express");
+    const app = express();
+    const handlebars = require('express-handlebars')
+    const bodyParser = require('body-parser')
+    const path = require("path")
+
+// Configurações
+
+
+    // Body Parser
+        app.use(bodyParser.urlencoded({extended: true}));
+        app.use(bodyParser.json());
+
+    // Handlebars
+        app.engine('handlebars', handlebars({defaultLayout: 'main'}));
+        app.set('view engine', 'handlebars');
+
+    // Public - Framework Bootstrap
+        app.use(express.static(path.join(__dirname, "public")))
+// Rotas
+
+    app.get('/', (req, res) => {
+        res.render('index')
+    })
+    app.get("/404", (req, res) => {
+        res.send("Error 404!")
+    })
+
+
+// Outros
+    const PORTA = process.env.PORT || 1501
+
+    app.listen(PORTA, function(){
+        console.log("O servidor está rolando na url http://localhost:1501!")
+    });
+
+    //Usuario
---
+++ public/js/texto.js
@@ -0,0 +1,83 @@
+function alfabeto(l){
+    if(l == 'a' || l == 'A'|| l == 'á' || l == 'â' || l == 'ã' || l == 'à' || l == 'ä' ||
+     l == 'å' || l == 'æ' || l == 'ā' || l == 'ă' || l == 'ą'){
+        return true;
+    }
+    else if(l == 'b' || l == 'B'){
+        return true;
+    }
+    else if(l == 'c' || l == 'C' || l == 'ç' || l == 'ć' || l == 'č'){
+        return true;
+    }
+    else if(l == 'd' || l == 'D' || l == 'ď' || l == 'đ'){
+        return true;
+    }
+    else if(l == 'e' || l == 'E' || l == 'é' || l == 'ê' || l == 'è' || l == 'ë' || l == 'ē' ||
+     l == 'ė' || l == 'ę' || l == 'ě' || l == 'ĕ' || l == 'ə'){
+        return true;
+    }
+    else if(l == 'f' || l == 'F'){
+        return true;
+    }
+    else if(l == 'g' || l == 'G' || l == 'ģ' || l == 'ğ'){
+        return true;
+    }
+    else if(l == 'h' || l == 'H'){
+        return true;
+    }
+    else if(l == 'i' || l == 'I' || l == 'í' || l == 'ì' || l == 'î' || l == 'ï' || l == 'ī' || l == 'į' || l == 'ı'){
+        return true;
+    }
+    else if(l == 'j' || l == 'J'){
+        return true;
+    }
+    else if(l == 'k' || l == 'K' || l == 'ķ'){
+        return true;
+    }
+    else if(l == 'l' || l == 'L' || l == 'ĺ' || l == 'ļ' || l == 'ľ' || l == 'ł'){
+        return true;
+    }
+    else if(l == 'm' || l == 'M'){
+        return true;
+    }
+    else if(l == 'n' || l == 'N' || l == 'ñ' || l == 'ń' || l == 'ņ' || l == 'ň'){
+        return true;
+    }
+    else if(l == 'o' || l == 'O' || l == 'ó' || l == 'ô' || l == 'õ' || l == 'ò' || l == 'ö' || l == 'ø' || l == 'ő' || l == 'œ'){
+        return true;
+    }
+    else if(l == 'p' || l == 'P'){
+        return true;
+    }
+    else if(l == 'q' || l == 'Q'){
+        return true;
+    }
+    else if(l == 'r' || l == 'R' || l == 'ŕ' || l == 'ř'){
+        return true;
+    }
+    else if(l == 's' || l == 'S' || l == 'ß' || l == '§' || l == 'ś' || l == 'š' || l == 'ş'){
+        return true;
+    }
+    else if(l == 't' || l == 'T' || l == 'þ' || l == 'ť' || l == 'ț' || l == 'ţ'){
+        return true;
+    }
+    else if(l == 'u' || l == 'U' || l == 'ú' || l == 'ü' || l == 'ù' || l == 'û' || l == 'ū' || l == 'ů' || l == 'ű' || l == 'ų'){
+        return true;
+    }
+    else if(l == 'v' || l == 'V'){
+        return true;
+    }
+    else if(l == 'x' || l == 'X'){
+        return true;
+    }
+    else if(l == 'y' || l == 'Y' || l == 'ý'){
+        return true;
+    }
+    else if(l == 'w' || l == 'W'){
+        return true;
+    }
+    else if(l == 'z' || l == 'Z' || l == 'ź' || l == 'ż' || l == 'ž'){
+        return true;
+    }
+    return false;
+}
